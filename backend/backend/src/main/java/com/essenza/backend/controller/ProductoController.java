41cb1f252dc929a842f66e1cb56998ac631aeb67
NicolasFarias33 <fariasnicolas557@gmail.com>
2026-07-07 23:34:25 -0300

    endpoints para obtener los productos y uno para crear un producto

---
+++ backend/backend/src/main/java/com/essenza/backend/controller/ProductoController.java
@@ -0,0 +1,27 @@
+package com.essenza.backend.controller;
+
+import com.essenza.backend.model.Producto;
+import com.essenza.backend.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping
+    public List<Producto> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    @PostMapping
+    public Producto crearProducto(@RequestBody Producto producto) {
+        return productoRepository.save(producto);
+    }
+}
---
+++ backend/backend/src/main/java/com/essenza/backend/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.essenza.backend.repository;
+
+import com.essenza.backend.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
